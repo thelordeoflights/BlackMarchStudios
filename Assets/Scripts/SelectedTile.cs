e77a67b0625d1db4a2298318066074f94e8ca085
Achal Wagadre <70798663+thelordeoflights@users.noreply.github.com>
2024-05-25 13:59:04 +0530

    Initial commit

---
+++ Assets/Scripts/SelectedTile.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SelectedTile : MonoBehaviour
+{
+    [HideInInspector] public Transform clickedTile;
+    void Update()
+    {
+
+        RaycastHit raycastHit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out raycastHit, 100f))
+        {
+            if (raycastHit.transform != null && raycastHit.transform == transform)
+            {
+                Debug.Log(transform.name);
+            }
+            if (Input.GetMouseButtonDown(0) && raycastHit.transform != null && raycastHit.transform == transform)
+            {
+                clickedTile = transform;
+                Debug.Log("clicked tile" + transform);
+            }
+        }
+
+    }
+
+}
